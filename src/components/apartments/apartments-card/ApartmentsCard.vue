c4f655002c2d982925637cb0ac097bf4189df96c
zubtsov_viktor@geos.local <zubtsov_viktor@smartworld.team>
2022-09-06 09:15:06 +0700

    Apartments

--- src/components/apartments/apartments-card/ApartmentsCard.vue
+++ src/components/apartments/apartments-card/ApartmentsCard.vue
@@ -21,12 +21,51 @@
       </div>
     </div>
     <apartments-photo :photos="apartment.images" />
-    <apartments-info :apartment="apartment" />
+    <apartments-info
+      v-if="!isMap"
+      :apartment="apartment"
+    />
+    <div
+      v-if="isMap"
+      class="map"
+      style="position: relative"
+    >
+      <google-map
+        api-key=""
+        style="width: 100%; height: 100%"
+        :center="currentPosition"
+        :zoom="15"
+      >
+        <marker
+          :options="{ position: currentPosition}"
+        />
+      </google-map>
+    </div>
+    <button
+      class="map-btn"
+      :class="{'map-btn-active': isMap}"
+      @click="() => isMap = !isMap"
+    >
+      <img
+        v-if="!isMap"
+        style="width: 18px; height: 18px"
+        src="./img/map.svg"
+        alt="map"
+      >
+      <img
+        v-else
+        style="width: 18px; height: 18px"
+        src="./img/close.svg"
+        alt="map"
+      >
+    </button>
   </article>
 </template>
 
 <script lang="ts">
 import { defineComponent, PropType } from 'vue'
+//@ts-ignore
+import { GoogleMap, Marker } from "vue3-google-map";
 import ApartmentsPhoto from "@/components/apartments/apartments-card/apartments-photo/ApartmentsPhoto.vue";
 import TApartment from "@/classes/apartments/TApartment";
 import ApartmentsInfo from "@/components/apartments/apartments-card/apartments-info/ApartmentsInfo.vue";
@@ -34,13 +73,29 @@ import UiText from "@/ui/ui-text/UiText.vue";
 
 export default defineComponent({
   name: 'ApartmentsCard',
-  components: { UiText, ApartmentsInfo, ApartmentsPhoto },
+  // eslint-disable-next-line vue/no-unused-components
+  components: { UiText, ApartmentsInfo, ApartmentsPhoto, GoogleMap, 'marker': Marker },
+  data () {
+    return {
+      isMap: false
+    }
+  },
   props: {
     apartment: {
       type: Object as PropType<TApartment>,
       required: true
     }
   },
+  computed: {
+    currentPosition () {
+      const [ lat, lng ] = this.apartment.coords.split(',');
+
+      return {
+        lat: +lat,
+        lng: +lng
+      }
+    }
+  }
 })
 </script>
 
